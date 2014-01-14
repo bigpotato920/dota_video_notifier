df2961955da9ede12fc5f9791e149f61ee79d3ed
bigpotato920 <732792753@qq.com>
2014-01-14 15:41:53 +0800

    check whether the web server use gzip to compress the web page while transfering to the user

--- notifier.py
+++ notifier.py
@@ -17,7 +17,12 @@ class Crawler:
 
 	
 	def read_config(self, filename):
+		"""
+		Read cofiguration from file
 
+		Args:
+			filename(): Filename of the configuration file
+		"""
 		file = open(filename)
 		while True:
 			line = file.readline()
@@ -35,17 +40,40 @@ class Crawler:
 
 	def get_page(self, url):
 
-		rawdata = urllib2.urlopen(url).read()
-		data = StringIO.StringIO(rawdata)
-		gz = gzip.GzipFile(fileobj=data)
-		content = gz.read()
-		gz.close()
+		"""
+		Get the content of a web page, judge whether the webserver use gzip to 
+		compress the web page, because the urllib2 library doesn't just whether
+		the server compress the page or not 
 
-		return content
+		Args:
+			url (str): Url of the target web page
 
+		Returns:
+			str: Content of the webpage
+		"""
+		f = urllib2.urlopen(url)
+		headers = f.info()
+		rawdata = f.read()
+
+		if ('content-Encoding' in headers and headers['Content-Encoding']) or \
+			('content-encoding' in headers and headers['content-encoding']):
+			data = StringIO.StringIO(rawdata)
+			gz = gzip.GzipFile(fileobj=data)
+			content = gz.read()
+			gz.close()
+
+			return content
+		return rawdata
 
 	def build_dict(self, content):
+		"""
+		Try to extract the title and the date of the update from the webpage using 
+		regular expression,build a dictionary to audit the title and date, namely 
+		(title, date) pairs
 
+		Args:
+			content (str): Content of the webpage
+		"""
 		pattern =  re.compile(r'<em>(.+)</em>')	
 		title_pattern = re.compile(r'(.+)<br')
 		date_pattern = re.compile(r'.+(\d{4}/\d{2}/\d{2})</font>')
@@ -62,34 +90,51 @@ class Crawler:
 				
 
 	def check_update(self):
+		"""
+		Check whether the content of the webpage is an update
+		"""
 		today = self.get_date_str()
 		msg = ''
 		for (title, date) in self.dict.items():
 			if date == today:
 				msg += title + '\n'
 				
-				
 		print msg
 		self.notify(msg)		
 		self.dict.clear()
 				
 	def notify(self, msg):
-		
+		"""
+		Notify the registered user there is an update through email
+
+		Args:
+			msg (str): Message to send to the target user
+		"""
 		self.email.send_email(self.user, self.pwd, msg, self.target)
   
+	
+	def get_date_str(self):
+		"""
+		Get the date string of today for example: 2014/12/05
+
+		Returns:
+			str: Date string of today following the format: %Y/%m/%d
+		"""
+		today = date.today()
+		return today.strftime("%Y/%m/%d")
+
 
 	def start(self):
-
+		"""
+		Start the service of the program
+		"""
 		self.email = Email()
 		content = self.get_page(self.url)
 		self.build_dict(content)
 		self.check_update()
 
 
-	def get_date_str(self):
 
-		today = date.today()
-		return today.strftime("%Y/%m/%d")
 		   		
 
 class Email():
@@ -98,7 +143,15 @@ class Email():
 		pass
 
 	def send_email(self, user, pwd, msg, target):
+		"""
+		Send an email to the target user
 
+		Args:
+			user (str): Account name of the sender
+			pwd (str): Password of the sender account
+			msg (str): Message send to the target user
+			target (str): Account of the target user
+		"""
 		msg = MIMEText(msg)
 		msg["Subject"] = "dota update"
 		msg["From"] = user
